95aa19885bf7b6b944a07c46f20c11e3a22bb78b
aditi-2004 <115215112+aditi-2004@users.noreply.github.com>
2024-11-25 20:48:41 +0530

    code
    complete code

--- backend/routes/resultsRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/resultsRoutes.js
-const express = require('express');
-const router = express.Router();
-const resultsController = require('../controllers/resultsController');
-const { adminAuth } = require('../middleware/authMiddleware');
-
-router.get('/', resultsController.getResults);
-
-module.exports = router;
